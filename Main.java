ab1e1279a0308bc4463a6a8e388803cb1d698405
Maxim Nani <39918125+Maximovix@users.noreply.github.com>
2018-12-25 19:58:47 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       new Thread( new Server("Server")).start();
-       Client client = new Client("Client");
+	   Client client = new Client("Client");
     }
 }
